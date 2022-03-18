0420f8a43d0fcf52317f1079f32327567f5ac6a5
Paweł Antczak <pawel@antczak.org>
2022-03-18 17:38:30 +0100

    Add new messages content

--- index.js
+++ index.js
@@ -1,80 +1,83 @@
-const messages = [
-    'Уважаемые россияне, ваши СМИ подвергаются цензуре.  Кремль лжет.  Узнайте правду об Украине в бесплатном Интернете и в приложении Telegram.  Время свергнуть диктатора Путина!',
-    'Кремль лжет!Война Путина принесла катастрофу народу России!В банках нет денег,нет топлива,все иностранные компании ушли из страны,а Путин прячется в своем дворце в Геленджике!',
-    'На Украине погибло больше сынов Русского Народа, чем в Афганистане! Война Путина уничтожит Россию! Топлива нет, денег в банках нет. Путин уничтожает Россию! Кремль лжет!',
-    'В Украине погибло более 10 000 сынов Русского Народа. Кремль лжет! Путин спрятался в своем дворце в Геленджике, но денег в банках нет, топлива нет, Россия обанкротилась. Вставай!',
-    'Ваши СМИ подвергаются цензуре. Кремль лжет. Ищите правду о войне в Украине в TOR: https://www.bbcnewsd73hkzno2ini43t4gblxvycyac5aw4gnv7t2rccijh7745uqd.onion/russian или используйте приложение Psiphon.'
-]
+const messages = ['Здравствуй, мой русский друг, Мы не знакомы, но я решил написать тебе. Я слышал, что ситуация в России начала осложняться из-за реакции на жестокое российское вторжение в Украину. Как ты?',
+  'Здравствуйте, мы не знакомы, но я решил написать вам, потому что постоянно читаю о войне в Украине. Как сейчас живется в России? Что вы слышите по телевизору о войне? Как ты?',
+  'Здравствуйте, мы не знакомы. Я живу за границей. Я знаю, что Россия вторглась в Украину, и там погибло много солдат и мирных жителей. Как вам живется в России? Как дела?',
+  'Привет! Как дела? Как сейчас живется в России? Я живу за границей и пишу вам, чтобы узнать, что вам говорят по телевизору о нападении России на Украину. Там гибнет много солдат и мирных жителей.']
+
 let messageIndex = 0;
 let currentMsg = '';
 let phoneNum = '';
 
 function iOS() {
-    return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
-    ].includes(navigator.platform)
-    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+      ].includes(navigator.platform)
+      || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 }
 
 function isMobile() {
-    var ua = navigator.userAgent;
-    return /Android|webOS|iPhone|iPad|iPod/i.test(ua);
+  var ua = navigator.userAgent;
+  return /Android|webOS|iPhone|iPad|iPod/i.test(ua);
 }
 
 async function draw() {
-    const response = await fetch('https://powiedz.im/api/'); //provide yours api endpoint, 1920.in won't work due to cors
-    phoneNum = await response.json();
-    setMessage();
+  const response = await fetch('https://powiedz.im/api/'); //provide yours api endpoint, 1920.in won't work due to cors
+  phoneNum = await response.json();
+  setMessage();
 }
 
-function setMessage()
-{
-    const phoneNumberField = document.getElementById("phoneNumber");
-    phoneNumberField.innerText = '+' + phoneNum;
-    let queryChar = iOS() ? '&' : '?';
-	let smshref = "sms:" + '+' + phoneNum + queryChar + "body=" + encodeURI(currentMsg);
-    phoneNumberField.href = smshref;
-    const sendButton = document.getElementById("buttonSend");
-    sendButton.href = smshref;
+function setMessage() {
+  const phoneNumberField = document.getElementById("phoneNumber");
+  phoneNumberField.innerText = '+' + phoneNum;
+  let queryChar = iOS() ? '&' : '?';
+  let smshref = "sms:" + '+' + phoneNum + queryChar + "body=" + encodeURI(
+      currentMsg);
+  phoneNumberField.href = smshref;
+  const sendButton = document.getElementById("buttonSend");
+  sendButton.href = smshref;
 }
 
 function getDefaultIndex() {
-    messageIndex = 0;
-    return messageIndex;
+  messageIndex = 0;
+  return messageIndex;
 }
 
 function drawText() {
-    currentMsg = messages[messageIndex == messages.length ? getDefaultIndex() : messageIndex];
-    messageIndex++;
-    const messageField = document.getElementById("message");
-    messageField.innerText = currentMsg;
-    setMessage()
+  currentMsg = messages[messageIndex == messages.length ? getDefaultIndex()
+      : messageIndex];
+  messageIndex++;
+  const messageField = document.getElementById("message");
+  messageField.innerText = currentMsg;
+  setMessage()
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    if (isMobile()) {
-        document.getElementById("buttonCopy").style = 'display: none';
-    } else {
-        document.getElementById("buttonSend").style = 'display: none';
-        document.getElementById("buttonCopy").addEventListener("click", () => { 
-            const phoneNumberField = document.getElementById("phoneNumber");
-            navigator.clipboard.writeText(phoneNumberField.innerText);
-        });
-    }
-    drawText();
-    const buttonReload = document.getElementById("buttonReload");
-    buttonReload.addEventListener("click", async () => { await draw() });
-    await draw();
-    
-    document.getElementById("buttonCopyText").addEventListener("click", () => {
-        const messageField = document.getElementById("message");
-        navigator.clipboard.writeText(messageField.innerText);
+  if (isMobile()) {
+    document.getElementById("buttonCopy").style = 'display: none';
+  } else {
+    document.getElementById("buttonSend").style = 'display: none';
+    document.getElementById("buttonCopy").addEventListener("click", () => {
+      const phoneNumberField = document.getElementById("phoneNumber");
+      navigator.clipboard.writeText(phoneNumberField.innerText);
     });
+  }
+  drawText();
+  const buttonReload = document.getElementById("buttonReload");
+  buttonReload.addEventListener("click", async () => {
+    await draw()
+  });
+  await draw();
 
-    document.getElementById("buttonTextReload").addEventListener("click", () => { drawText() });
+  document.getElementById("buttonCopyText").addEventListener("click", () => {
+    const messageField = document.getElementById("message");
+    navigator.clipboard.writeText(messageField.innerText);
+  });
+
+  document.getElementById("buttonTextReload").addEventListener("click", () => {
+    drawText()
+  });
 });
